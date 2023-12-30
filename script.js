a7870a8cd7215d7b6eb5faf37cca4067c2c186bb
unknown <bhairavs501@gmail.com>
2023-12-30 12:11:27 +0530

    test-1

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-AOS.init();
+window.addEventListener('scroll', reveal)
+
+function reveal(){
+    var reveal = document.querySelectorAll('.box')
+    console.log(reveal)
+    for(var i = 0; i < reveal.length; i++){
+        var windowHeight = window.innerHeight;
+        var revealTop = reveal[i].getBoundingClientRect().top
+        var revealPoint = 150
+        console.log(windowHeight-revealPoint)
+        if(revealTop < (windowHeight-revealPoint)){
+            reveal[i].classList.add('active');
+        } else{
+            reveal[i].classList.remove('active')
+        }
+    }
+}
 
 $(document).ready(function(){
     $('.toggle').click(function() {
