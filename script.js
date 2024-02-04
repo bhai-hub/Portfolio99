aba9352a3adfcb922d95aefd7327e656ea759f0c
unknown <bhairavs501@gmail.com>
2024-02-04 15:00:01 +0530

    Updated

--- script.js
+++ script.js
@@ -50,5 +50,48 @@ $(document).ready(function(){
 
     }
     
+    const obj = [
+        {
+            "img": 'assets/IndoSac.JPG',
+            "title" : 'IndoSac - Spice up your moments with our Indian Snack Symphony!',
+            "link" : 'https://stirring-crepe-69d3d1.netlify.app/'
+        },
+        {
+            "img": 'assets/wallpaper.JPG',
+            "title" : 'Wally - webpage built using pixbay api',
+            "link" : 'https://bhai-hub.github.io/wally/'
+        },
+        {
+            "img": 'assets/weather.JPG',
+            "title" : 'Weathery : API based application using which we can monitor multi location weather',
+            "link" : 'https://bhai-hub.github.io/weather103/'
+        }
+    ]
 
+    console.log(obj)
+
+    const proj = document.getElementById("proj");
+
+    for (var i = 0; i < obj.length; i++) {
+        let html = `<div class="cardp">
+            <div class="card-content">
+                <img src="${obj[i].img}" alt="">
+            </div>
+            <div class="info">
+                <p>${obj[i].title}</p>
+                <a href="${obj[i].link}" target="_blank">Visit</a>
+            </div>
+        </div>`;
+        proj.innerHTML += html;
+    }
     
+
+//     <div class="cardp">
+//     <div class="card-content">
+//       <img src="assets/TinDog.JPG" alt="">
+//     </div>
+//     <div class="info">
+//       <p>Tindog - Landing page for dating app for dog</p>
+//       <a href="https://bhai-hub.github.io/TinDog/">Vist</a>
+//     </div>
+//   </div>
